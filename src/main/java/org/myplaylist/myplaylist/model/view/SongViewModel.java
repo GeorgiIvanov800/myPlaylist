0bb036a2023bb746fc0d8c21054a0260bae0374a
givanov <gogata92@pm.me>
2023-11-25 23:31:22 +0100

    add duration field to the view model

--- src/main/java/org/myplaylist/myplaylist/model/view/SongViewModel.java
+++ src/main/java/org/myplaylist/myplaylist/model/view/SongViewModel.java
@@ -1,10 +1,13 @@
 package org.myplaylist.myplaylist.model.view;
 
+import java.time.Duration;
+
 public record SongViewModel(
      Long id,
      String title,
      String artist,
      String album,
      String genre,
-     String filePath
+     String filePath,
+     Duration duration
 ) {}
