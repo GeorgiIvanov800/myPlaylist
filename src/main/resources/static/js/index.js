853f5b3cd0df9a68fcc96c4b98a3c9998e076713
givanov <gogata92@pm.me>
2023-12-05 16:26:30 +0100

    Some refactoring

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,7 @@
 function ratePlaylist(playlistId, ratingType) {
 
+    console.log("hello from index");
+
     let csrfTokenMeta = document.querySelector('meta[name="_csrf"]');
     let csrfHeaderMeta = document.querySelector('meta[name="_csrf_header"]');
 
@@ -36,48 +38,3 @@ function updateRatingCount(playlistId, likeCount, dislikeCount) {
     likeCountElement.textContent = likeCount;
     dislikeCountElement.textContent = dislikeCount;
 }
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     document.querySelectorAll('.show-more').forEach(function (link) {
-//         link.addEventListener('click', function (event) {
-//             event.preventDefault();
-//
-//             // Get the ID of the song list associated with this link
-//             let songListId = this.getAttribute('data-song-list');
-//             let songList = document.querySelector(songListId);
-//             let hiddenItems = songList.querySelectorAll('.hidden');
-//             let separators = songList.querySelectorAll('.song-separator');
-//
-//             if (this.textContent.includes('more')) {
-//                 // Show all hidden songs in this playlist
-//                 hiddenItems.forEach(item => item.classList.remove('hidden'));
-//
-//                 // Change link text to "Show Less"
-//                 this.textContent = '... and less';
-//
-//                 // Show separators, except the last one
-//                 separators.forEach(separator => separator.style.display = 'block');
-//                 if (separators.length > 0) {
-//                     separators[separators.length - 1].style.display = 'none';
-//                 }
-//             } else {
-//                 // Hide songs again in this playlist
-//                 hiddenItems.forEach(item => item.classList.add('hidden'));
-//
-//                 // Change link text to "Show More"
-//                 this.textContent = '... and more';
-//
-//                 // Hide all separators
-//                 separators.forEach(separator => separator.style.display = 'none');
-//             }
-//         });
-//     });
-//
-//     // Initially hide the last separator in each song list
-//     document.querySelectorAll('.song-list').forEach(function (songList) {
-//         let separators = songList.querySelectorAll('.song-separator');
-//         if (separators.length > 0) {
-//             separators[separators.length - 1].style.display = 'none';
-//         }
-//     });
-// });
